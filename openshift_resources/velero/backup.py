1a24775a4d462f3dba8fc4cc973db2e5221d0aac
amastbau <amastbau@redhat.com>
2022-08-24 14:18:04 +0300

    Fixing  Adding resource handlers

--- openshift_resources/velero/backup.py
+++ openshift_resources/velero/backup.py
@@ -3,12 +3,4 @@ from constants.resources import VELERO_API_GROUP
 
 
 class Backup(NamespacedResource):
-
     api_group = VELERO_API_GROUP
-
-    def __init__(self,
-                 hooks,
-                 namespaces_to_include,
-                 storage_location,
-                 ):
-        pass
