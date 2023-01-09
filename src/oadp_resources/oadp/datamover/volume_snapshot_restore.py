f7e78b46fd62971531d5791331ccc1def00fb19f
Amos Mastbaum <amastbau@redhat.com>
2023-01-09 06:23:33 +0200

    Extending VolumesnapshotRestore API Class

--- src/oadp_resources/oadp/datamover/volume_snapshot_restore.py
+++ src/oadp_resources/oadp/datamover/volume_snapshot_restore.py
@@ -2,5 +2,37 @@ from ocp_resources.resource import NamespacedResource
 from oadp_constants.resources import ApiGroups
 
 
-class VolumeSnapshotLocation(NamespacedResource):
+class VolumeSnapshotRestore(NamespacedResource):
     api_group = ApiGroups.DATAMOVER_OADP_API_GROUP.value
+
+    def is_done(self):
+        """
+        Check is VSR process is done
+        @return: True if the VSR process is not running; False otherwise
+        """
+        return self.instance.status and self.instance.status.phase != \
+            VolumeSnapshotRestorePhase.SnapMoverRestorePhaseInProgress.value
+
+    @classmethod
+    def get_by_restore_name(cls, restore_name):
+        """
+        Returns a list of VSRs by restore name
+        @param restore_name: the restore name to get the VSR/s by
+        @return: returns a list of VSR/s by restore name restore_name; empty list otherwise
+        """
+        return list(cls.get(label_selector=f"velero.io/restore-name={restore_name}"))
+
+    @classmethod
+    def get_by_source_pvc(cls, src_pvc_name: str, vsr_list: list = None):
+        """
+        Returns a list of VSRs by source PVC
+        @param src_pvc_name: PVC name which the VSR/s point to
+        @param vsr_list: the list of VSR/s to filter based on the PVC name; in case not provided, it will be retrieved
+        @return: a list of VSRs by source PVC; empty list otherwise
+        """
+        if not vsr_list:
+            vsr_list = list(cls.get())
+        vsr_filtered_list = list(filter(
+            lambda x: x.instance.spec.volumeSnapshotMoverBackupRef.sourcePVCData.name == src_pvc_name, vsr_list))
+
+        return vsr_filtered_list
