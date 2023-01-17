ca9d0bd29783de0b29a6f79e51f78f9ab474a135
Amos Mastbaum <amastbau@redhat.com>
2023-01-17 11:31:34 +0200

    More Extentions

--- src/oadp_resources/velero/backup.py
+++ src/oadp_resources/velero/backup.py
@@ -1,6 +1,13 @@
 from oadp_constants.resources import ApiGroups
 from ocp_resources.resource import NamespacedResource
+from oadp_constants.velero.backup import BackupPhase
 
 
 class Backup(NamespacedResource):
     api_group = ApiGroups.VELERO_API_GROUP.value
+
+    def completed(self):
+        try:
+            return self.status.phase == BackupPhase.BackupPhaseCompleted.value
+        except AttributeError:
+            return False
