4fd18c9f259a3e6e1add10cb5754b6c40a416d58
Shahaf Bahar <sbahar@redhat.com>
2023-02-14 14:40:20 +0200

    Add more information to logs in check_phase function

--- src/oadp_utils/phase.py
+++ src/oadp_utils/phase.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 def check_phase(resource, expected_phase):
     try:
         current_phase = resource.instance.status.phase
-        logger.info(f"Current phase status is: {current_phase}")
+        logger.info(f"Current phase status of the resource {resource.kind}/{resource.name} is: {current_phase}")
         return current_phase == expected_phase
     except AttributeError:
         return False
